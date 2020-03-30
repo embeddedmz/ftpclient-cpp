0c33b72f642153592e1b132e9ff148d9d9f53dfc
Sven Fink <sven.fink@wipotec.com>
2020-03-30 10:13:11 +0200

    Check error code on curl_init

--- FTP/CurlHandle.cpp
+++ FTP/CurlHandle.cpp
@@ -1,10 +1,16 @@
 #include "CurlHandle.h"
 
 #include <curl/curl.h>
+#include <stdexcept>
 
 namespace embeddedmz {
 
-CurlHandle::CurlHandle() { curl_global_init(CURL_GLOBAL_ALL); }
+CurlHandle::CurlHandle() {
+  const auto eCode = curl_global_init(CURL_GLOBAL_ALL);
+  if (eCode != CURLE_OK) {
+    throw std::runtime_error{"Error initializing libCURL"};
+  }
+}
 
 CurlHandle::~CurlHandle() { curl_global_cleanup(); }
 
